d5c567fc3ca5deafedaacdaa8c53a725530bc4c9
root <root@DESKTOP-QMNTID1.localdomain>
2020-04-28 11:25:35 +0900

    webpack config modify

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: ["@babel/polyfill", "./src/index.js"],
   output: {
     publicPath: "/",
     filename: "bundle.js",
